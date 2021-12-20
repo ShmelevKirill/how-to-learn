e45ce78e9e892e823aee333c9c68dc7ef4a0cf5c
ShmelevKirill <96147610+ShmelevKirill@users.noreply.github.com>
2021-12-20 23:03:28 +0300

    Add files via upload
    footer

---
+++ script.js
@@ -0,0 +1,6 @@
+$(function(){
+
+    $('.banner-section__slider').slick({
+     prevArrow: '<button class="banner-section__slider-btn banner-section__slider-btnprev"></button>',
+     nextArrow: '<button class="banner-section__slider-btn banner-section__slider-btnnext"><img src="images/arrow-right.png" alt=""></button>',
+    });
